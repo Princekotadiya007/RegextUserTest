56eea35554fe89ba41ecac44556d444ecd57d1ae
Princekotadiya007 <princuuu77@gmail.com>
2023-03-01 13:26:28 +0530

    [prince]:add Email valid or Not

--- RegexUserRegistration/TestUserRegex/UnitTest1.cs
+++ RegexUserRegistration/TestUserRegex/UnitTest1.cs
@@ -15,7 +15,13 @@ namespace TestUserRegex
         [Test]
         public void LasttName_ValidOrNot()
         {
-            string result = regex.ValidateLastName("Prince");
+            string result = regex.ValidateLastName("Kotadiya");
+            Assert.AreEqual("Valid", result);
+        }
+        [Test]
+        public void Email_ValidOrNot()
+        {
+            string result = regex.ValidateEmail("abc.xyz@bridgelabz.co.in");
             Assert.AreEqual("Valid", result);
         }
     }
